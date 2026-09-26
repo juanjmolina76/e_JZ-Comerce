77b5608c087b050005ad0e254fc6b4c91e9b0fa8
juanjmolina76 <molinajuanj@hotmail.com>
2026-09-26 09:19:52 -0300

    estilos e imagen en productos.json

--- e_JZ-Comerce/src/Components/Products/Item.jsx
+++ e_JZ-Comerce/src/Components/Products/Item.jsx
@@ -1,11 +1,14 @@
 import estilo from '../Items.module.css';    
 
-const Item  = ({ nombre, precio, descripcion }) => {
+const Item  = ({ nombre, precio, descripcion, stock, imagen}) => {
   return (
     <div className={estilo.item}>
       <h2 >{nombre}</h2>
+      <img src={('./datos/images/' + imagen)} alt={nombre} />
       <p>{descripcion}</p>
       <p>Precio: AR${precio}</p>
+      <p>Stock: {stock}</p>
+     
     </div>
     );
 };
